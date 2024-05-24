34250219bcb524a54cf977f31379e4951d3a8dea
Maximo Mayo <108980824+MaximoMayo@users.noreply.github.com>
2024-05-24 04:27:10 -0700

    commenting comments

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,19 +1,23 @@
 "use strict";
 
+//importing the arrays from other js files to use on this one
 import { locationsArray } from './locationData.js';
 import { nationalParksArray } from './nationalParkData.js';
 import { parkTypesArray } from './parkTypeData.js';
 
+//putting global variables and initalizing the javascript
 document.addEventListener("DOMContentLoaded", init);
 const typeFilter = document.getElementById("typeFilter");
 const parkFilter = document.getElementById("parkFilter");
 const resultParkContainer = document.getElementById("resultParkContainer");
 
+//this will preform the dropdown and add event listeners to dynamically change the type filter and dropdown
 function init() {
     populateDropdownParks();
     setupEventListenersParks();
 }
 
+//this function will determine if the type is either loc or type and use their respective dropdowns
 function populateDropdownParks(){
     parkFilter.innerHTML = "";
     if(typeFilter.value == "location"){
@@ -24,6 +28,7 @@ function populateDropdownParks(){
     }
 }
 
+//loc dropdown
 function populateDropdownParksLocation() { 
     const fragment = document.createDocumentFragment(); 
     locationsArray.forEach((location) => {
@@ -33,6 +38,7 @@ function populateDropdownParksLocation() {
     parkFilter.appendChild(fragment);
 }
 
+//type dropdown
 function populateDropdownParksType() { 
     const fragment = document.createDocumentFragment(); 
   
@@ -43,6 +49,7 @@ function populateDropdownParksType() {
     parkFilter.appendChild(fragment);
 }
 
+//this event listener will make the dropdown and the filter functions dynamic
 function setupEventListenersParks(){
     typeFilter.addEventListener("change", function() {
         populateDropdownParks();
@@ -50,8 +57,11 @@ function setupEventListenersParks(){
     });
 }
 
+//this will determine from the typeFilter whether it should use the loc table or type table
 function selectFilterFunction() {
     const selectedFilter = typeFilter.value;
+
+    //this removes so that the site remains dynamic by letting a different filter replace the last one
     parkFilter.removeEventListener("change", filterParkLocation);
     parkFilter.removeEventListener("change", filterParkType);
 
@@ -66,18 +76,21 @@ function selectFilterFunction() {
     }
 }
 
+//will filter by the chosen state and where its included in the nationalParksArray
 function filterParkLocation(){
     const selectedLocation = parkFilter.value;
     const filteredParks = nationalParksArray.filter(park => park.State.includes(selectedLocation));
     displayFilteredParks(filteredParks, ["LocationID", "LocationName", "Address", "Phone", "Visit"]);
 }
 
+//will filter by the chosen LocationName and where its included in the nationalParksArray
 function filterParkType(){
     const selectedType = parkFilter.value;
     const filteredParks = nationalParksArray.filter(park => park.LocationName.includes(selectedType));
     displayFilteredParks(filteredParks, ["LocationID", "LocationName", "Address", "Phone", "Visit"]);
 }
 
+//this function focuses on reporting if there is no park and by combining the last two functions to send to the html
 function displayFilteredParks(filteredParks, fields){
     resultParkContainer.innerHTML = "";
 
@@ -92,6 +105,7 @@ function displayFilteredParks(filteredParks, fields){
     resultParkContainer.appendChild(table);
 }
 
+//this function creates the table and make a layout so that the next function can fill it out
 function createTable(fields){
     const table = document.createElement("table");
     table.classList.add("park-table");
@@ -107,6 +121,7 @@ function createTable(fields){
     return table;
 }
 
+//this function will add all of the information needed into the table
 function populateTableRows(table, parks, fields){
     parks.forEach(park => {
         const row = document.createElement("tr");
